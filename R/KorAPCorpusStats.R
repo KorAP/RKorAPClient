636fd393b30c46321a9ae80ea2ce6fe4ba944cc3
Marc Kupietz <kupietz@ids-mannheim.de>
2019-09-12 17:58:23 +0200

    KorAPCorpusStats: update documentation
    Change-Id: I7506a029f3ded9f5fca1ea53d4eb3c465a3ede1a

--- R/KorAPCorpusStats.R
+++ R/KorAPCorpusStats.R
@@ -1,7 +1,14 @@
 #' @import jsonlite
 #' @import curl
 
+dummy <- NA # without this roxygen2 fails
+
+#' Fetch information about a (virtual) corpus
+#' @param con object obtained from \code{\link{KorAPConnection}}
+#' @param query object returned from \code{\link{KorAPQuery}}
+#' @param vc string describing the virtual corpus. An empty string (default) means the whole corpus, as far as it is license-wise accessible.
 #' @export
+#' @return object with the fields \code{$documents}, \code{$tokens}, \code{$sentences}, \code{$paragraphs}
 KorAPCorpusStats <- function(con, vc = NA, query = NA) {
   if ((is.na(query) && is.na(vc)) || !(is.na(query) || is.na(vc))) {
     stop("Exaclty one of the parameters query and vc must be specified.")
