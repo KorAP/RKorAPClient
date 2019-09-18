e9182c25004066d0d82bf24a91b807a21948b286
Marc Kupietz <kupietz@ids-mannheim.de>
2019-09-18 22:09:33 +0200

    Add example alternativesOverTime.R
    Change-Id: I2e64f1adfa84463e3738551c5cc908cb80de2edd

---
+++ examples/alternativesOverTime.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+#
+# Plot frequency of alternative expressions or spellings variants over time
+#
+library(RKorAPClient)
+library(ggplot2)
+library(reshape2)
+
+alternativesOverTime <- function(alternatives, years, kco = new("KorAPConnection", verbose=TRUE)) {
+  df = data.frame(year=years)
+  vc = "textType = /Zeitung.*/ & pubDate in"
+  for (v in alternatives) {
+    df[v] <- sapply(df$year, function(y)  corpusQuery(kco, query=v, vc=paste(vc, y))@totalResults)
+  }
+  df$total <- apply(df[,alternatives], 1, sum)
+  df <- melt(df, measure.vars = alternatives, value.name = "afreq", variable.name = "variant")
+  df$ci <- t(sapply(Map(prop.test, df$afreq, df$total), "[[","conf.int"))
+  df$share <- df$afreq / df$total
+  g <- ggplot(data = df, mapping = aes(x = year, y = share, color=variant)) +
+    geom_point() +
+    geom_line() +
+    geom_errorbar(aes(ymin=ci[, 1], ymax=ci[, 2]), width=.5, alpha=.5) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))  + scale_x_continuous(breaks=unique(df$year))
+  print(g)
+  df
+}
+
+df <- alternativesOverTime(c("so genannter", "sogenannter"), (1995:2018))
