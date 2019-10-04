6a6c4ba2775286d921a0a74cbeaf726c62858802
Marc Kupietz <kupietz@ids-mannheim.de>
2019-10-04 09:05:04 +0200

    Add some package documentation
    Change-Id: I38a8c3d8f5c02f40f0944fbfc668fa8638a9fbbe

---
+++ R/RKorAPClient.R
@@ -0,0 +1,10 @@
+#' \code{RKorapClient} package
+#'
+#' R pakcage to access the \href{https://github.com/KorAP/}{KorAP} web service API.
+#'
+#' See the README.md on
+#' \href{https://github.com/KorAP/RKorAPClient/}{github}
+#'
+#' @docType package
+#' @name RKorAPClient
+NULL
