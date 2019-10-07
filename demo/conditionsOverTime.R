603a50cc2b924b6559b0ef3c39a7040e894f64ac
Marc Kupietz <kupietz@ids-mannheim.de>
2019-10-07 19:29:44 +0200

    Add convenience bindings to ggplot2 and plotly with hyperlinks
    Change-Id: Iaecd23b1401bc5df7ee4a904dca2e87466266143

--- demo/conditionsOverTime.R
+++ demo/conditionsOverTime.R
@@ -6,17 +6,19 @@
 #install_git("https://korap.ids-mannheim.de/gerrit/KorAP/RKorAPClient", upgrade="never")
 library(RKorAPClient)
 library(ggplot2)
+library(plotly)
 
 conditionsOverTime <- function(query, conditions, years, kco = new("KorAPConnection", verbose = TRUE)) {
   g <- expand_grid(condition = conditions, year = years) %>%
     cbind(frequencyQuery(kco, query, sprintf("%s & pubDate in %d", .$condition, .$year))) %>%
     ipm() %>%
-    ggplot(aes(x = year, y = ipm, fill=condition, color=condition, ymin=conf.low, ymax=conf.high)) +
+    ggplot(aes(x = year, y = ipm, fill=condition, color=condition)) +
     geom_freq_by_year_ci() +
     xlab("TIME") +
     labs(color="Virtual Corpus", fill="Virtual Corpus") +
-    ylab(sprintf("Observed frequency/million of \u201c%s\u201d", query))
-  print(g)
+    ylab(sprintf("Observed frequency of \u201c%s\u201d", query))
+  p <- RKorAPClient::ggplotly(g)
+  print(p)
 }
 
 conditionsOverTime("[tt/l=Heuschrecke]", c("textClass = /natur.*/", "textClass=/politik.*/", "textClass=/wirtschaft.*/"), (2002:2018))
