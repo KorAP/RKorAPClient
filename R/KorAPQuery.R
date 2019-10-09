0c29cea17f880a92972b6a1c5369bcb7fe414706
Marc Kupietz <kupietz@ids-mannheim.de>
2019-10-09 08:44:36 +0200

    Add confidence level argument to frequencyQuery
    Change-Id: I2ca64e8a5aa7027c46382d2531e32f0d672c9810

--- R/KorAPQuery.R
+++ R/KorAPQuery.R
@@ -303,12 +303,13 @@ setMethod("fetchRest", "KorAPQuery", function(kqo, verbose = kqo@korapConnection
 #'
 #' @param kco \code{\link{KorAPConnection}} object (obtained e.g. from \code{new("KorAPConnection")}
 #' @param query string that contains the corpus query. The query language depends on the \code{ql} parameter. Either \code{query} must be provided or \code{KorAPUrl}.
+#' @param conf.level confidence level of the returned confidence interval (passed throgh \code{\link{ci}}  to \code{\link{prop.test}}).
 #' @export
 setMethod("frequencyQuery", "KorAPConnection",
-  function(kco, query, vc = "", ...) {
-      corpusQuery(kco, query, vc, metadataOnly = TRUE, as.df=TRUE, ...) %>%
+  function(kco, query, vc = "", conf.level = 0.95, ...) {
+      corpusQuery(kco, query, vc, metadataOnly = TRUE, as.df = TRUE, ...) %>%
       mutate(tokens=corpusStats(kco, vc=vc, as.df=TRUE)$tokens) %>%
-      ci()
+      ci(conf.level = conf.level)
 })
 
 #´ format()
