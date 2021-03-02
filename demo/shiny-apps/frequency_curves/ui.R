fc7151815d29e9ef60121308ba8700e6b00326b7
Marc Kupietz <kupietz@ids-mannheim.de>
2021-03-02 08:12:59 +0100

    shiny-demo: show sliders for start and end year
    Change-Id: I19eabbc6c3e1f2b4de64d54a4c790a64987b1d32

--- demo/shiny-apps/frequency_curves/ui.R
+++ demo/shiny-apps/frequency_curves/ui.R
@@ -20,9 +20,21 @@ shinyUI(
                                     placeholder = "Mit Komma getrennte Liste")),
         column(
           width = 1,
+          offset = 0,
           style = "margin-top: 25px;",
           actionButton("goButton", "Mit KorAP suchen", icon("search"), class = "btn btn-primary")
+        ),
+        column(width = 2, offset=3,
+          sliderInput("from", "Von", sep = "",
+                    min = 1949, max = 2020,
+                    value = 2005)
+        ),
+        column(width = 2, offset=0,
+               sliderInput("to", "Bis", sep = "",
+                           min = 1949, max = 2020,
+                           value = 2020)
         )
+
       ),
       fluidRow(height = 10,
                highchartOutput("hcontainer", height = "600px"))
