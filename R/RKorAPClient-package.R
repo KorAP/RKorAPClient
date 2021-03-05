97a352fdc3a71bdcdc4ecf4d1f5ab5b2e3525775
Marc Kupietz <kupietz@ids-mannheim.de>
2021-03-05 10:50:32 +0100

    More documentation improvements
    Change-Id: I34484d6df2e91eceb809aa97d189f8ea4918d8ea

--- R/RKorAPClient-package.R
+++ R/RKorAPClient-package.R
@@ -1,5 +1,13 @@
 #' @references
-#' Kupietz, Marc / Diewald, Nils / Margaretha, Eliza (2020): RKorAPClient: An R package for accessing the German Reference Corpus DeReKo via KorAP. In: Calzolari, Nicoletta, Frédéric Béchet, Philippe Blache, Khalid Choukri, Christopher Cieri,  Thierry Declerck, Sara Goggi, Hitoshi Isahara, Bente Maegaard, Joseph Mariani, Hélène Mazo, Asuncion Moreno, Jan Odijk, Stelios Piperidis (eds.): [Proceedings of The 12th Language Resources and Evaluation Conference (LREC 2020)](http://www.lrec-conf.org/proceedings/lrec2020/LREC-2020.pdf). Marseille: European Language Resources Association (ELRA), 7017-7023. <http://www.lrec-conf.org/proceedings/lrec2020/pdf/2020.lrec-1.867.pdf>
+#' Kupietz, Marc / Diewald, Nils / Margaretha, Eliza (2020):
+#' RKorAPClient: An R package for accessing the German Reference Corpus DeReKo
+#' via KorAP. In: Calzolari, Nicoletta, Frédéric Béchet, Philippe Blache,
+#' Khalid Choukri, Christopher Cieri,  Thierry Declerck, Sara Goggi,
+#' Hitoshi Isahara, Bente Maegaard, Joseph Mariani, Hélène Mazo,
+#' Asuncion Moreno, Jan Odijk, Stelios Piperidis (eds.):
+#' Proceedings of The 12th Language Resources and Evaluation Conference (LREC 2020)
+#' Marseille: European Language Resources Association (ELRA), 7017-7023.
+#' \url{http://www.lrec-conf.org/proceedings/lrec2020/pdf/2020.lrec-1.867.pdf}
 #'
 #' @keywords internal
 "_PACKAGE"
