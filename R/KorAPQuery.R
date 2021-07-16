ffd2330e2c1f61cb631b12b01863b81d4c5ea101
Marc Kupietz <kupietz@ids-mannheim.de>
2021-07-16 13:20:56 +0200

    Add prototypical full collocation analysis
    Change-Id: I898169b2790ff7e67771db45be0eee7c0035de3f

--- R/KorAPQuery.R
+++ R/KorAPQuery.R
@@ -168,7 +168,7 @@ setMethod("corpusQuery", "KorAPConnection",
       }
       request <-
         paste0('?q=',
-               URLencode(query, reserved = TRUE),
+               URLencode(as.character(query), reserved = TRUE),
                if (vc != '') paste0('&cq=', URLencode(vc, reserved = TRUE)) else '', '&ql=', ql)
       webUIRequestUrl <- paste0(kco@KorAPUrl, request)
       requestUrl <- paste0(
@@ -450,7 +450,8 @@ setMethod("collocationScoreQuery", "KorAPConnection",
                    leftContextSize = 5,
                    rightContextSize = 5,
                    scoreFunctions = defaultAssociationScoreFunctions(),
-                   smoothingConstant = .5
+                   smoothingConstant = .5,
+                   observed = NULL
                    ) {
             # https://stackoverflow.com/questions/8096313/no-visible-binding-for-global-variable-note-in-r-cmd-check
             O1 <- O2 <- O <- N <- E <- w <- 0
@@ -490,12 +491,12 @@ setMethod("collocationScoreQuery", "KorAPConnection",
               collocate = collocate,
               label = queryStringToLabel(vc),
               vc = vc,
-              webUIRequestUrl = frequencyQuery(kco, query, vc)$webUIRequestUrl,
+              webUIRequestUrl = if(is.null(observed)) frequencyQuery(kco, query, vc)$webUIRequestUrl else rep("", length(observed)),
               w = leftContextSize + rightContextSize,
               leftContextSize,
               rightContextSize,
               N  = frequencyQuery(kco, node, vc)$total + smoothingConstant,
-              O = as.double(frequencyQuery(kco, query, vc)$totalResults) + smoothingConstant,
+              O = as.double( if(is.null(observed)) frequencyQuery(kco, query, vc)$totalResults else observed) + smoothingConstant,
               O1 = frequencyQuery(kco, node, vc)$totalResults + smoothingConstant,
               O2 = frequencyQuery(kco, collocate, vc)$totalResults + smoothingConstant,
               E = w * as.double(O1) * O2 / N
---
+++ demo/ca.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+library(RKorAPClient)
+kco <- new("KorAPConnection", verbose = TRUE)
+
+snippet2FreqTable <- function(snippet,
+                              minOccurr = 5,
+                              leftContextSize = 5,
+                              rightContextSize = 5,
+                              stopwords = c("der", "die", "das", "ein", "eine", "einer", "den", "dem", "des", "einen", "von", "mit", "zu", "und"),
+                              oldTable = data.frame(word = rep(NA, 1), frequency = rep(NA, 1))) {
+  if (length(snippet) > 1) {
+    message(paste("Joinging", length(snippet), "kwics"))
+    for (s in snippet) {
+      oldTable <- snippet2FreqTable(
+        s,
+        leftContextSize = 5,
+        rightContextSize = 5,
+        oldTable = oldTable
+      )
+    }
+    stopwordsTable <- tibble(word=stopwords)
+    oldTable  %>%
+      filter(frequency >= minOccurr & word != "NA") %>%
+      arrange(desc(frequency)) %>%
+      anti_join(stopwordsTable, by="word")
+
+  } else {
+    match <-
+      str_match(
+        snippet,
+        '<span class="context-left"><span class="more"></span>(.*[^ ]) *</span><span class="match"><mark>.*</mark></span><span class="context-right"> *([^<]*)'
+      )
+    left <- tail(match[1, 2], leftContextSize)
+    right <- head(match[1, 3], rightContextSize)
+    str_split(paste(left, right), "([! )(»«),.:?„“\'\"]+|&quot;)")[[1]] %>%
+      table() %>%
+      as.data.frame() %>%
+      dplyr::rename(word = 1, frequency = 2) %>%
+      filter(str_detect(word, '^[:alnum:]+[:alnum:-]$')) %>%
+      full_join(oldTable, y, by = "word") %>%
+      filter(!is.na(word)) %>%
+      replace_na(list(frequency.x = 0, frequency.y = 0)) %>%
+      mutate(frequency = frequency.x + frequency.y) %>%
+      select(word, frequency)
+  }
+}
+
+
+collocatesQuery <-
+  function(kco,
+           query,
+           minOccur = 5,
+           leftContextSize = 5,
+           rightContextSize = 5,
+           limit = NA,
+           ...) {
+    q <- corpusQuery(kco, query, metadataOnly = F, ...) %>%
+      fetchNext(maxFetch=limit)
+    df <- snippet2FreqTable((q@collectedMatches)$snippet,
+                      minOccur = minOccur,
+                      leftContextSize = leftContextSize,
+                      rightContextSize = leftContextSize)
+  }
+
+collocationAnalysis <-
+  function(kco,
+           query,
+           minOccur = 5,
+           leftContextSize = 5,
+           rightContextSize = 5,
+           maxCollocates = 200,
+           limit = NA,
+           ...) {
+    candidates <- collocatesQuery(kco, query, minOccur = minOccur,
+                    leftContextSize = leftContextSize,
+                    rightContextSize = leftContextSize, limit, ...) %>%
+                  head(maxCollocates)
+    print(candidates)
+    collocationScoreQuery(kco, node=query, collocate=candidates$word, leftContextSize = leftContextSize,
+                          rightContextSize = leftContextSize, observed=candidates$frequency, ...)
+    }
+
+kco <- new("KorAPConnection", verbose = TRUE)
+if (is.null(kco@accessToken)) {
+  message()
+}
+
+collocationAnalysis(kco,"Ameisenplage") %>% arrange(desc(logDice))
+
