c82527b940b861b678caafd0e8cd2257920c5314
Marc Kupietz <kupietz@ids-mannheim.de>
2022-02-24 12:49:28 +0100

    Skip tests that require internet if offline
    Change-Id: I679362d9d3595c8212c20d2086d92afcbd58d13f

--- tests/testthat/test-corpusStats.R
+++ tests/testthat/test-corpusStats.R
@@ -1,4 +1,5 @@
 test_that("corpusStats works", {
+  skip_if_offline()
   stats <- new("KorAPConnection") %>% corpusStats()
   expect_gt(stats@tokens, 0)
   expect_gt(stats@paragraphs, 0)
@@ -7,11 +8,13 @@ test_that("corpusStats works", {
 
 
 test_that("Printing corpusStats for the whole corpus works", {
+  skip_if_offline()
   stats <- new("KorAPConnection") %>% corpusStats()
   expect_error(print(stats), NA)
 })
 
 test_that("Printing corpusStats for a sub-corpus works", {
+  skip_if_offline()
   stats <- new("KorAPConnection") %>% corpusStats("pubDate in 2018")
   expect_error(print(stats), NA)
 })
