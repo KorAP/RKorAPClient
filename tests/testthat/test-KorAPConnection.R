9a4cdba40d5c8cdd264b0ffc2ed3c5c7bb9e221f
Marc Kupietz <kupietz@ids-mannheim.de>
2023-04-16 14:23:05 +0200

    Don't test graceful fails with http://httpbin.org when offline
    Change-Id: I1c5e953b2c9a30361da5df06d16e5e3379d1bbf9

--- tests/testthat/test-KorAPConnection.R
+++ tests/testthat/test-KorAPConnection.R
@@ -1,27 +1,33 @@
 test_that("KorAPConnection fails gracefully on unresolvable host", {
+  skip_if_offline()
   expect_message(new("KorAPConnection", apiUrl="http://xxx.asdhsahdsadhvgas.org"), "No internet|Could not resolve")
 })
 
 test_that("KorAPConnection fails gracefully on timeout", {
+  skip_if_offline()
   expect_message(new("KorAPConnection", apiUrl="http://httpbin.org/delay/3", timeout = 1), "No internet|Timeout")
 })
 
 test_that("KorAPConnection is printable", {
+  skip_if_offline()
   kco <- new("KorAPConnection", timeout = 10)
   skip_if(is.null(kco@welcome))
   expect_error(print(kco), NA)
 })
 
 test_that("Opening KorAPConnection prints some message.", {
+  skip_if_offline()
   expect_message(new("KorAPConnection"), "KorAP")
 })
 
 test_that("Opening KorAPConnection with invalid apiToken fails gracefully", {
+  skip_if_offline()
   expect_message(new("KorAPConnection", accessToken="test token", timeout = 10),
                "401|Timeout")
 })
 
 test_that("Persisting null apiToken fails", {
+  skip_if_offline()
   kco <- new("KorAPConnection", timeout = 10)
   skip_if_not(is.null(kco@accessToken))
   skip_if(is.null(kco@welcome))
@@ -31,6 +37,7 @@ test_that("Persisting null apiToken fails", {
 })
 
 test_that("Opening KorAPConnection with KorAPUrl works", {
+  skip_if_offline()
   kco <- new("KorAPConnection", KorAPUrl="https://korap.ids-mannheim.de", timeout = 1)
   expect_equal(kco@apiUrl, paste0("https://korap.ids-mannheim.de/api/", kco@apiVersion, "/"))
   kco <- new("KorAPConnection", KorAPUrl="https://korap.ids-mannheim.de/", timeout = 1)
