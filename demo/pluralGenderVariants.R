9aade05f3edb848fa477a0d23ee5e035123bc5fe
Marc Kupietz <kupietz@ids-mannheim.de>
2023-05-12 17:30:07 +0200

    pluralGenderVariants demo: oauthorize demo if necessary
    Change-Id: I9b166410c7c2ffb657c8f6448086f3d43b723dbf

--- demo/pluralGenderVariants.R
+++ demo/pluralGenderVariants.R
@@ -43,13 +43,23 @@ unravelPunctuationGenderCases <- function(df, suffix = "innen", kco = new("KorAP
   }
 }
 
+oauthorizeDemo <- function(kco) { # if no access token is set ask for one for the demo application
+  if (kco@accessToken == "") {
+    kco@accessToken <- oauth_client(id = "773NHGM76N7P9b6rLfmpM4",
+                                    token_url = paste0(kco@apiUrl, "oauth2/token")) %>%
+      oauth_flow_auth_code( scope = "search match_info",
+                            auth_url = paste0(kco@KorAPUrl, "settings/oauth/authorize"))
+  }
+  kco
+}
+
 plotPluralGenderVariants <- function(word = "Nutzer",
                           years = c(1995:2022),
                           as.alternatives = FALSE,
                           vc = "referTo ratskorpus-2023-1 & pubDate in",
                           suffixes = c('Innen', '[\\*]innen"', '[_]innen"', ' innen'),
                           prefixes = c('',      '"',            '"',        ''),
-                          kco = new("KorAPConnection", verbose=TRUE) ) {
+                          kco = new("KorAPConnection", verbose=TRUE) %>% oauthorizeDemo()) {
   hc <-
     frequencyQuery(kco, paste0(prefixes, word, suffixes), paste(vc, years), as.alternatives=as.alternatives) %>%
     unravelPunctuationGenderCases(kco = kco) %>%
@@ -59,8 +69,9 @@ plotPluralGenderVariants <- function(word = "Nutzer",
 }
 
 
+
 hc <- plotPluralGenderVariants("Nutzer", c(1995:2022), as.alternatives = FALSE)
-# htmlwidgets::saveWidget(hc, file=fname, selfcontained = TRUE)
+htmlwidgets::saveWidget(hc, file="/tmp/NutzerInnen.html", selfcontained = TRUE)
 
 # Diewald, Nils/Kupietz, Marc/Lüngen, Harald (2022):
 # Tokenizing on scale. Preprocessing large text corpora on the lexical and sentence level.
