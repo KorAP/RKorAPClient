bbe0ab2ab5fe5cea7fd121945af3513164e5bedd
Marc Kupietz <kupietz@ids-mannheim.de>
2023-05-13 17:30:15 +0200

    oauthorize only if necessary
    Change-Id: Ie1e4218a74c882a4f7e37238847562815fddd4b3

--- demo/OAuthBrowserflowHttr2.R
+++ demo/OAuthBrowserflowHttr2.R
@@ -2,12 +2,27 @@ library(RKorAPClient)
 library(httr2)
 library(kableExtra)
 
-token <- oauth_client( id = "773NHGM76N7P9b6rLfmpM4",
-    token_url = "https://korap.ids-mannheim.de/api/v1.0/oauth2/token") %>%
-  oauth_flow_auth_code( scope = "search match_info",
-    auth_url = "https://korap.ids-mannheim.de/settings/oauth/authorize")
+demo_kor_app_id = "773NHGM76N7P9b6rLfmpM4"
 
-new("KorAPConnection", verbose = TRUE, accessToken = token$access_token) %>%
+oauthorizeDemo <- function(kco, app_id = demo_kor_app_id) {
+  if (is.null(kco@accessToken) || is.null(kco@welcome)) { # if access token is not set or invalid
+    kco@accessToken <- ( # request one
+      oauth_client(
+        id =  app_id, # for the demo application
+        token_url = paste0(kco@apiUrl, "oauth2/token")
+      ) %>%
+        oauth_flow_auth_code(
+          scope = "search match_info",
+          auth_url = paste0(kco@KorAPUrl, "settings/oauth/authorize")
+        )
+    )$access_token
+  }
+  print(kco@accessToken)
+  return(kco)
+}
+
+new("KorAPConnection", verbose = TRUE) %>%
+  oauthorizeDemo() %>%
   collocationAnalysis("focus([marmot/p=ADJA] {Gendern})", leftContextSize=1, rightContextSize=0) %>%
   mutate(collocate = paste0('<a href="', webUIRequestUrl, '">', collocate, '</a>')) %>%
   select(collocate, O, pmi, mi2, mi3, logDice, ll) %>%
