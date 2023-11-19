94b52c4f1ebbcbbc847b44d4db9c868113f1d492
Marc Kupietz <kupietz@ids-mannheim.de>
2023-11-19 11:45:30 +0100

    Fix url encoding of ampersand in corpusStats value
    Change-Id: I17e72a979df5ed69f8de2fd762dd33fd121214c9

--- tests/testthat/test-corpusStats.R
+++ tests/testthat/test-corpusStats.R
@@ -1,11 +1,20 @@
 test_that("corpusStats works", {
   skip_if_offline()
-  stats <- new("KorAPConnection") %>% corpusStats()
+  stats <- new("KorAPConnection") %>% corpusStats("pubDate since 2020 & pubDate until 2021")
   expect_gt(stats@tokens, 0)
   expect_gt(stats@paragraphs, 0)
   expect_gt(stats@documents, 0)
+  expect(grepl("%26", stats@webUIRequestUrl), "webUIRequestUrl not properly url encoded")
 })
 
+test_that("corpusStats with result as df works", {
+  skip_if_offline()
+  stats <- new("KorAPConnection") %>% corpusStats("pubDate since 2020 & pubDate until 2021", as.df = TRUE)
+  expect_gt(stats$tokens, 0)
+  expect_gt(stats$paragraphs, 0)
+  expect_gt(stats$documents, 0)
+  expect(grepl("%26", stats$webUIRequestUrl), "webUIRequestUrl not properly url encoded")
+})
 
 test_that("Printing corpusStats for the whole corpus works", {
   skip_if_offline()
@@ -15,6 +24,6 @@ test_that("Printing corpusStats for the whole corpus works", {
 
 test_that("Printing corpusStats for a sub-corpus works", {
   skip_if_offline()
-  stats <- new("KorAPConnection") %>% corpusStats("pubDate in 2018")
+  stats <- new("KorAPConnection") %>% corpusStats("pubDate since 2020 & pubDate until 2021")
   expect_error(print(stats), NA)
 })
