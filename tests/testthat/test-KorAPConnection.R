b1dec01760a046f5ab627301619b8c57097a8164
Marc Kupietz <kupietz@ids-mannheim.de>
2025-06-04 17:16:57 +0200

    Code cleanup
    Change-Id: I084eb212ae4a1fdbc563e686ea6c9de55fc75914

--- tests/testthat/test-KorAPConnection.R
+++ tests/testthat/test-KorAPConnection.R
@@ -1,13 +1,13 @@
 test_that("KorAPConnection fails gracefully on unresolvable host", {
-  expect_message(KorAPConnection(accessToken = NULL, apiUrl="http://xxx.asdhsahdsadhvgas.org"), "No internet|Could not resolve")
+  expect_message(KorAPConnection(accessToken = NULL, apiUrl = "http://xxx.asdhsahdsadhvgas.org"), "No internet|Could not resolve")
 })
 
 test_that("KorAPConnection fails gracefully on timeout", {
-  expect_message(KorAPConnection(apiUrl="http://httpbin.org/delay/3", accessToken = NULL, timeout = 0.2), "No internet|Timeout|json|progress|Unavailable")
+  expect_message(KorAPConnection(apiUrl = "http://httpbin.org/delay/3", accessToken = NULL, timeout = 0.2), "No internet|Timeout|json|progress|Unavailable")
 })
 
 test_that("KorAPConnection fails gracefully on Bad Gateway errors", {
-  expect_message(KorAPConnection(apiUrl="http://httpbin.org/status/502", accessToken = NULL, timeout = 0.5), "No internet|Timeout|progress|json|502")
+  expect_message(KorAPConnection(apiUrl = "http://httpbin.org/status/502", accessToken = NULL, timeout = 0.5), "No internet|Timeout|progress|json|502")
 })
 
 test_that("KorAPConnection is printable", {
@@ -21,8 +21,10 @@ test_that("Opening KorAPConnection prints some message.", {
 })
 
 test_that("Opening KorAPConnection with invalid apiToken fails gracefully", {
-  expect_message(KorAPConnection(accessToken="test token", timeout = 3),
-               "401|Timeout|progress")
+  expect_message(
+    KorAPConnection(accessToken = "test token", timeout = 3),
+    "401|Timeout|progress"
+  )
 })
 
 test_that("Persisting null apiToken fails", {
@@ -30,13 +32,14 @@ test_that("Persisting null apiToken fails", {
   skip_if_not(is.null(kco@accessToken))
   skip_if(is.null(kco@welcome))
   expect_error(persistAccessToken(kco),
-               ".*not supplied any access token.*",
-               perl = TRUE)
+    ".*not supplied any access token.*",
+    perl = TRUE
+  )
 })
 
 test_that("Opening KorAPConnection with KorAPUrl works", {
-  kco <- KorAPConnection(accessToken = NULL, KorAPUrl="https://korap.ids-mannheim.de", timeout = 1)
+  kco <- KorAPConnection(accessToken = NULL, KorAPUrl = "https://korap.ids-mannheim.de", timeout = 1)
   expect_equal(kco@apiUrl, paste0("https://korap.ids-mannheim.de/api/", kco@apiVersion, "/"))
-  kco <- KorAPConnection(accessToken = NULL, KorAPUrl="https://korap.ids-mannheim.de/", timeout = 1)
+  kco <- KorAPConnection(accessToken = NULL, KorAPUrl = "https://korap.ids-mannheim.de/", timeout = 1)
   expect_equal(kco@apiUrl, paste0("https://korap.ids-mannheim.de/api/", kco@apiVersion, "/"))
 })
