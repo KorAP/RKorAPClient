b7a3805feea1a5a969293b4273fc828c19bf80fb
Marc Kupietz <kupietz@ids-mannheim.de>
2025-06-24 16:53:22 +0200

    Simplify package documentation
    Change-Id: Ib00464ed762cc229e6bf6f000da88092ce0a6281

--- R/RKorAPClient-package.R
+++ R/RKorAPClient-package.R
@@ -1,3 +1,47 @@
+#' R Client for KorAP Corpus Analysis Platform
+#'
+#' @description
+#' RKorAPClient provides programmatic access to KorAP corpus analysis platform instances,
+#' enabling corpus linguistic research on large corpora like DeReKo, CoRoLa, DeLiKo@@DNB.
+#'
+#' @section Main Functions:
+#' \describe{
+#'   \item{Connection}{`KorAPConnection()`, `auth()`, `persistAccessToken()`}
+#'   \item{Search}{`corpusQuery()`, `fetchAll()`, `fetchNext()`}
+#'   \item{Analysis}{`corpusStats()`, `frequencyQuery()`, `collocationAnalysis()`}
+#' }
+#'
+#' @section Quick Start:
+#' ```r
+#' library(RKorAPClient)
+#' # Connect and search
+#' kcon <- KorAPConnection()
+#' query <- corpusQuery(kcon, "Ameisenplage")
+#' results <- fetchAll(query)
+#'
+#' # Access results
+#' results@collectedMatches
+#' results@totalResults
+#' ```
+#'
+#' @section Common Workflows:
+#'
+#' **Basic Search:**
+#' ```r
+#' kcon <- KorAPConnection()
+#' kcon |> corpusQuery("search term") |> fetchAll()
+#' ```
+#'
+#' **Frequency Analysis:**
+#' ```r
+#' frequencyQuery(kcon, c("term1", "term2"), vc="pubDate in 2020")
+#' ```
+#'
+#' **Corpus Statistics:**
+#' ```r
+#' corpusStats(kcon, vc="textType=Zeitung", as.df=TRUE)
+#' ```
+#'
 #' @references
 #' Kupietz, Marc / Diewald, Nils / Margaretha, Eliza (2020):
 #' RKorAPClient: An R package for accessing the German Reference Corpus DeReKo
